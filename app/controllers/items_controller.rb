de62ad829588be6f5af6c3085c126d0647674547
Sylvain Peigney <sylvain@volpeo.fr>
2015-12-15 11:50:37 +0100

    breadcrumb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def show
     @chapter = Chapter.find(params[:chapter_id])
+    @course = Course.find(params[:course_id])
     @item = Item.find(params[:id])
   end
 end
